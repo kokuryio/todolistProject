74ba20df607933e2a92949593532d0c0578a8c13
Alina Holz <135610412+kokuryio@users.noreply.github.com>
2024-01-07 15:19:49 +0200

    Update RemoteTodoAccessor.java
    Just some minor comments

--- backend/src/main/java/de/thb/fbi/msr/maus/todolist/remote/RemoteTodoAccessor.java
+++ backend/src/main/java/de/thb/fbi/msr/maus/todolist/remote/RemoteTodoAccessor.java
@@ -2,7 +2,6 @@ package de.thb.fbi.msr.maus.todolist.remote;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.log4j.Logger;
 import de.thb.fbi.msr.maus.todolist.model.Todo;
 import de.thb.fbi.msr.maus.todolist.model.TodoCRUDAccessor;
@@ -12,11 +11,13 @@ public class RemoteTodoAccessor implements TodoCRUDAccessor {
 	protected static Logger logger = Logger
 			.getLogger(RemoteTodoAccessor.class);
 
+	//This is where we put our Todo objects
 	private static final List<Todo> sItemList = new ArrayList<Todo>();
 
 
 	@Override
 	public List<Todo> readAllTodos() {
+		//Just log that we read our items, for debugging purposes
 		logger.info("readAllTodos(): " + sItemList);
 
 		return sItemList;
@@ -33,9 +34,7 @@ public class RemoteTodoAccessor implements TodoCRUDAccessor {
 		logger.info("deleteTodo(): " + itemId);
 
 		boolean removed = sItemList.remove(new Todo() {
-			/**
-			 * 
-			 */
+
 			private static final long serialVersionUID = 71193783355593985L;
 
 			@Override
